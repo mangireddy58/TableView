248c08d9f447ec1a25c75a09a69128f9359517bf
Sai Praveen Kumar <saipraveenkumark@gmail.com>
2018-10-17 11:06:15 +0530

    Initial Commit

---
+++ TestTblView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTblView
+//
+//  Created by Sai Praveen Kumar on 17/10/18.
+//  Copyright © 2018 Mangi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
